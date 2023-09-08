bd8b1eb35cef98b5c5837d7a85f7677c654ad4d1
ashulme96 <anshulme96@.com>
2023-09-08 23:54:13 +0530

    Lets go live

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 var cors = require('cors')
 app.use(cors())
-
+const fs=require('fs')
 require('dotenv').config();
-
+const path=require('path')
 const {sequelize} = require('./database/squelize');
 const {Expense} = require('./model/expenditure');
 const {User} = require('./model/login');
@@ -16,9 +16,19 @@ const expenseRoutes = require('./router/router.js')
 const purchaseRoute=require('./router/purchase.js')
 const premium=require('./router/premium')
 const forgotpassword=require('./router/forgetPassward');
+const helmet=require('helmet')
+const compression=require('compression');
+const morgan=require('morgan')
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({ extended: false }));
+
+const accesslog_stream=fs.createWriteStream(path.join(__dirname,'access.log'),
+{flag:'a'}
+);
+app.use(helmet())
+app.use(compression())
+app.use(morgan('combined',{stream:accesslog_stream}))
 
 app.use(express.static('./frontEnd/expense'));
 app.use('/', expenseRoutes)
